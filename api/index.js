3ef1ea89ea836519b7a8171e9e53128db28de76a
Laura Villarroel <lauramar.villarroel@gmail.com>
2022-06-27 13:11:14 -0500

    cambios

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ let CreateDBTypes = async () => {
 };
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001');
     CreateDBTypes();
